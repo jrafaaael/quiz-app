818643cc9cd0eb14bd578b3f542cfec91f1f4fd9
jrafaaael <jrmd070102@gmail.com>
2021-06-12 20:54:54 -0400

    removed code and files generated by vue-cli

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,29 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+    <router-view />
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import url('https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&family=Roboto:wght@100;300;400;500;700;900&display=swap');
+*,
+*::after,
+*::before {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+
+    font-family: 'Lato', sans-serif;
+    letter-spacing: 0.125rem;
 }
 
-#nav {
-  padding: 30px;
+html,
+body {
+    width: 100%;
+    min-height: 100vh;
+    color: #333;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+button {
+    background-color: transparent;
+    border: none;
 }
 </style>
