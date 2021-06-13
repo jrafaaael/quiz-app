318eb0b0ad55e7640fae00c6da06ec14508fb874
jrafaaael <jrmd070102@gmail.com>
2021-06-12 20:39:14 -0400

    added .svg icons

---
+++ src/assets/icons/index.js
@@ -0,0 +1,9 @@
+import './minus.svg'
+import './plus.svg'
+
+import './atom.svg'
+import './baseball-outline.svg'
+import './dice-outline.svg'
+import './film.svg'
+import './globe-americas.svg'
+import './head-side-brain.svg'
