2d09238325ccf0273826b0c30953ca31597eaa13
jrafaaael <jrmd070102@gmail.com>
2021-06-12 20:57:49 -0400

    created data for categories (extracted from api) and its icons

---
+++ src/utils/categories.js
@@ -0,0 +1,34 @@
+const CATEGORIES = [
+    {
+        name: "Random",
+        id: '',
+        iconName: 'dice-outline'
+    },
+    {
+        name: "General Knowledge",
+        id: 9,
+        iconName: "head-side-brain",
+    },
+    {
+        name: "Film",
+        id: 11,
+        iconName: "film",
+    },
+    {
+        name: "Science & Nature",
+        id: 17,
+        iconName: "atom",
+    },
+    {
+        name: "Sports",
+        id: 21,
+        iconName: "baseball-outline",
+    },
+    {
+        name: "Geography",
+        id: 22,
+        iconName: "globe-americas",
+    },
+]
+
+export default CATEGORIES;
