3469384af0f168b0270322e3de68d058cb17dc88
jrafaaael <jrmd070102@gmail.com>
2021-06-12 20:43:44 -0400

    added multiple dependencies

--- src/main.js
+++ src/main.js
@@ -2,5 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VWave from "v-wave";
 
-createApp(App).use(store).use(router).mount('#app')
+import './assets/icons'
+
+createApp(App)
+.use(VWave, {
+    color: "rgba(255, 255, 255, 0.75)"
+})
+.use(store)
+.use(router)
+.mount('#app')
