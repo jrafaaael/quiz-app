159902b6fae62d03938478c8c30df53ad157a497
jrafaaael <jrmd070102@gmail.com>
2021-06-22 12:03:44 -0400

    game route. fetching questions. preventing url changes in game

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home/Home.vue'
+import Game from "../views/Game/Game.vue";
+
+import store from "@/store";
 
 const routes = [
   {
@@ -7,11 +10,35 @@ const routes = [
     name: 'Home',
     component: Home
   },
+  {
+    path: '/question/:id',
+    name: 'Game',
+    component: Game,
+    params: true
+  }
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.afterEach(async (to, from) => {
+  if(to.path.includes('question')) {
+    if (to.params.id === "1") await store.dispatch("GAME/getQuestions");
+    else document.querySelector("input:checked").checked = false;
+  }
+});
+
+router.beforeEach((to, from) => {
+  if(to.path.includes('question') && to.params.id != store._state.data.GAME.QUESTION) {
+    return {
+        name: "Game",
+        params: {
+            id: store._state.data.GAME.QUESTION,
+        },
+    };
+  }
 })
 
 export default router
