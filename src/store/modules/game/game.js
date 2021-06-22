375a8fcbf89192fcefc038db36116a329d5054c0
jrafaaael <jrmd070102@gmail.com>
2021-06-22 11:56:10 -0400

    created game vuex module

--- src/store/modules/game/game.js
+++ src/store/modules/game/game.js
@@ -1,13 +1,75 @@
+// IMPORTS
+import axios from "axios";
+
+import {
+    setCurrentQuestionNumber,
+    setQuestions,
+    setGameState,
+    increaseScore,
+    resetScore,
+} from "./mutation-types";
+
 // VARIABLES
 const GAME = {
     namespaced: true,
     state: {
+        QUESTION: 1,
+        QUESTIONS: null,
+        GAME_STATE: "finished",
+        SCORE: 0,
     },
     mutations: {
+        [setCurrentQuestionNumber](state, payload) {
+            state.QUESTION = payload;
+        },
+        [setQuestions](state, payload) {
+            state.QUESTIONS = payload;
+        },
+        [setGameState](state, payload) {
+            state.GAME_STATE = payload;
+        },
+        [increaseScore](state, payload) {
+            state.SCORE = payload;
+        },
+        [resetScore](state, payload) {
+            state.SCORE = payload;
+        }
     },
     actions: {
-    }
-}
+        async getQuestions({ rootState, dispatch, commit }) {
+            try {
+                const res = await axios.get("https://opentdb.com/api.php?", {
+                    params: {
+                        amount: rootState.GAME_CONFIG.NUMBER_OF_QUESTIONS,
+                        category: rootState.GAME_CONFIG.CATEGORY,
+                        difficulty: rootState.GAME_CONFIG.DIFFICULTY,
+                        type: "multiple",
+                    },
+                });
+                const data = await res.data;
+                dispatch("setQuestions", data.results);
+                commit("setGameState", "playing");
+            } catch (error) {
+                console.log(error);
+            }
+        },
+        setQuestions({ commit }, payload) {
+            commit("setQuestions", payload);
+        },
+        setCurrentQuestionNumber({ commit }, payload) {
+            commit("setCurrentQuestionNumber", payload);
+        },
+        setGameState({ commit }, payload) {
+            commit("setGameState", payload);
+        },
+        increaseScore({ commit, state }) {
+            commit("increaseScore", state.SCORE + 1);
+        },
+        resetScore({ commit }) {
+            commit("resetScore", 0);
+        },
+    },
+};
 
 // EXPORTS
 export default GAME;
---
+++ src/store/modules/game/mutation-types.js
@@ -0,0 +1,5 @@
+export const setQuestions = "setQuestions";
+export const setCurrentQuestionNumber = "setCurrentQuestionNumber";
+export const setGameState = "setGameState";
+export const increaseScore = "increaseScore";
+export const resetScore = "resetScore";
