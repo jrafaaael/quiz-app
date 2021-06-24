78a28655a7079fcaf2b6dfd531ec3bcbf502a9bf
jrafaaael <jrmd070102@gmail.com>
2021-06-24 13:15:09 -0400

    added path to deploy

--- vue.config.js
+++ vue.config.js
@@ -3,20 +3,24 @@ const SpriteLoaderPlugin = require('svg-sprite-loader/plugin')
 module.exports = {
     chainWebpack: (config) => {
         config
-            .plugin('svg-sprite-loader')
-            .after('html')
+            .plugin("svg-sprite-loader")
+            .after("html")
             .use(SpriteLoaderPlugin)
             .tap(() => [{ plainSprite: true }]);
 
         config.module
-            .rule('svg-sprite-loader')
+            .rule("svg-sprite-loader")
             .test(/icons.*\.svg$/)
-            .use('svg-sprite-loader')
-            .loader('svg-sprite-loader')
+            .use("svg-sprite-loader")
+            .loader("svg-sprite-loader")
             .end()
-            .use('svgo-loader')
-            .loader('svgo-loader');
+            .use("svgo-loader")
+            .loader("svgo-loader");
 
-        config.module.rule('svg').exclude.add(/icons/);
+        config.module.rule("svg").exclude.add(/icons/);
     },
+    publicPath:
+        process.env.NODE_ENV === "production"
+            ? "/covid-data-tracker-vuejs/"
+            : "/",
 };
